7613e2f8fcfcccf58b82523ffecc2fd5c4595045
santosheranki <santosherankicse90@gmail.com>
2022-11-21 14:40:25 +0530

    modified server

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+   console.log('Server is up!');
+});
