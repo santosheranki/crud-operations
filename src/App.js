a47970a8c46f8c1b07609a3ce2e56ec563b8be18
santosheranki <santosherankicse90@gmail.com>
2022-11-21 17:41:18 +0530

    appjs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact  path="/" element={<Login/>}/>
+        <Route exact path="/" element={<Login/>}/>
         <Route exact path="/dashboard" element={<Dashboard/>}/>
         <Route exact path="/students" element={<Student/>}/>
       </Routes>
